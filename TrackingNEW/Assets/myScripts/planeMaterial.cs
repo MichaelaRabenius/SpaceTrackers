69425e48bdccbe0bc42eba8195c0ca68ce0ea508
ViktorIgeland <viktor_igeland@hotmail.com>
2017-03-06 15:16:25 +0100

    Lägger till trackingprojektet
    trackingprojektet

---
+++ TrackingNEW/Assets/myScripts/planeMaterial.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planeMaterial : MonoBehaviour {
+
+	public TextAsset imageAsset;
+	// Use this for initialization
+	void Start () {
+		Texture2D tex = new Texture2D (2, 2);
+		tex.LoadImage (imageAsset.bytes);
+		GetComponent<Renderer> ().material.mainTexture = tex;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
