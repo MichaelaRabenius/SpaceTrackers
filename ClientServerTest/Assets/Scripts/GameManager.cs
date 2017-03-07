9fd7871932cbc895b10418540e99e8613389e3f5
Jacob Andersson <jacob.m.andersson@gmail.com>
2017-03-07 09:41:19 +0100

    Basic lobby alpha functionality

---
+++ ClientServerTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance {set; get;}
+
+	public KandNetworkManager kandNetworkManager;
+
+	public GameObject mainMenu;
+
+	public GameObject hostServerMenu;
+	public InputField inputJoinGameServer;
+
+	public GameObject joinGameMenu;
+	public GameObject connectingMenu;
+	public GameObject lobbyMenu;
+
+	void Start () {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+
+		ShowMenu (mainMenu);
+	}
+
+	public void Update() {
+		// If host server menu
+		if (hostServerMenu.activeInHierarchy) {
+			Text connectionsCountLabel = GameObject.FindGameObjectWithTag ("ConnectionsCount").GetComponent<Text> ();
+			connectionsCountLabel.text = "Connections: " + kandNetworkManager.GetConnectionsCount ();
+		}
+	}
+
+	public void ShowMenu(GameObject menu) {
+		mainMenu.SetActive (false);
+		hostServerMenu.SetActive (false);
+		joinGameMenu.SetActive (false);
+		connectingMenu.SetActive (false);
+		lobbyMenu.SetActive (false);
+
+		menu.SetActive (true);
+	}
+
+	// START Server part
+	public void HostGame() {
+		// Start server
+		kandNetworkManager.HostGame ();
+		ShowMenu(hostServerMenu);
+
+		// Show server ip in menu
+		Text serverLabel = GameObject.FindGameObjectWithTag ("ServerHostname").GetComponent<Text> ();
+		string ip = kandNetworkManager.GetHostingIP ();
+		serverLabel.text = ip;
+	}
+
+	public void ShutdownServer() {
+		// Shutdown server
+		kandNetworkManager.ShutdownServer ();
+	}
+	// END Server part
+
+	// START Client part
+	public void JoinGame() {
+		string serverInput = inputJoinGameServer.text;
+		kandNetworkManager.JoinServer (serverInput);
+
+		ShowMenu (connectingMenu);
+	}
+
+	public void OnFailedToConnect(NetworkConnectionError error) {
+		if (kandNetworkManager.isClient)
+			ShowMenu (joinGameMenu);
+	}
+
+	public void OnConnectedToServer() {
+		ShowMenu (lobbyMenu);
+	}
+	// END Client part
+}
---
+++ ClientServerTest/Assets/Scripts/KandNetworkManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class KandNetworkManager : NetworkBehaviour {
+
+	public GameManager gameManager;
+
+	public int port = 7777;
+	private string serverIp;
+
+	// START Server part
+	public void HostGame() {
+		// Start server
+		bool useNat = !Network.HavePublicAddress ();
+		Network.InitializeServer(8, port, useNat);
+
+		Debug.Log ("Server has been initialized");
+	}
+
+	public void ShutdownServer() {
+		// Shutdown server
+		Network.Disconnect();
+
+		Debug.Log ("Server has been shutdown");
+	}
+	// END Server part
+
+	// START Client part
+	public void JoinServer(string sIP) {
+		serverIp = sIP;
+		Network.Connect (serverIp, port);
+
+		Debug.Log ("Connecting to: " + serverIp);
+	}
+
+	public void OnFailedToConnect(NetworkConnectionError error) {
+		Debug.Log("Could not connect to server: " + error);
+	}
+
+	public void OnConnectedToServer() {
+		Debug.Log ("Connected to: " + serverIp);
+	}
+	// END Client server part
+
+	// START Getters
+	public string GetHostingIP() {
+		return Network.player.ipAddress;
+	}
+
+	public int GetConnectionsCount() {
+		return Network.connections.Length;
+	}
+
+	public string GetServerIp() {
+		return serverIp;
+	}
+	// END Getters
+
+}
