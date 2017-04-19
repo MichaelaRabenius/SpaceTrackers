fcff0095a3ce303c9aa86b4527c0485e39b60914
Jacob Andersson <jacob.m.andersson@gmail.com>
2017-04-19 10:17:50 +0200

    Sync

---
+++ RetryLobby2.0/Assets/LobbyScene/ForceOrientation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceOrientation : MonoBehaviour {
+	
+	void Start() {
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+	}
+
+}
